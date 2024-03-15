6f09f83f9aea03dcfea82bf2a1ff66e3f90546e1
ritz89 <shs500@umkt.ac.id>
2024-03-15 11:00:11 +0800

    update base

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_widget_lecture/ui/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: const Dashboard(),
     );
   }
 }
